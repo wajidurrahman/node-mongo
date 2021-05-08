1defbd49a6aa1603d92027ebf16e875b77e1e096
Wajidur Rahman <wrfuad0@gmail.com>
2021-05-08 18:15:21 +0600

    git commit -m

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const express = require('express');
 
 const app = express();
 
+const users = ["Asad", "Moin", "Sabed", 'Susmita', 'Sohana']
+
 app.get ('/', (req, res) =>{
-    res.send("Thank you for Calling me");
+   const fruit = {
+       product: 'ada',
+       price:' 220'
+   }
+
+    res.send(fruit);
+})
+app.get('/fruits/banana', (req,res) => {
+    res.send({fruit:'banana', quantity: 1000, price:100000});
 })
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query.sort);
+    const name = users[id];
+    res.send({id, name});
+})
+
+app.listen(42000, () => console.log('Listening to port 42000'));
