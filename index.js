99f22860c1e7fa2dd589d415afcce8a89e3eae29
Wajidur Rahman <wrfuad0@gmail.com>
2021-05-10 00:23:11 +0600

    Node express module summary

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const express = require('express');
-
-
+const  cors = require('cors');
+const bodyParser = require('body-parser')
 const app = express();
 
-const users = ["Asad", "Moin", "Sabed", 'Susmita', 'Sohana']
+app.use(cors());
+app.use(bodyParser.json())
+
+const users = ["Asad", "Moin", "Sabed", 'Susmita', 'Sohana', 'Sabana'];
 
 app.get ('/', (req, res) =>{
    const fruit = {
        product: 'ada',
        price:' 220'
    }
-
     res.send(fruit);
-})
-app.get('/fruits/banana', (req,res) => {
-    res.send({fruit:'banana', quantity: 1000, price:100000});
-})
+});
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
-    console.log(req.query.sort);
     const name = users[id];
     res.send({id, name});
 })
 
-app.listen(42000, () => console.log('Listening to port 42000'));
+// post 
+app.post('/addUser',(req, res) => {
+    // save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
+
+app.listen(4200, () => console.log('Listening to port 4200'));
